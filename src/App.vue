84171c3b3f09682fda8c5e906096a86597652900
h3huan <leaupo@163.com>
2024-05-02 22:39:03 +0800

    h3huan微瑕

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="http://www.yanhuangxueyuan.com/threejs/files/favicon.ico" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/fav.ico" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="合欢与合欢花" />
